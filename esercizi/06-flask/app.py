75cb9bbe2e0768470c752dc18901781a605f3949
Luigi De Russis <luigidr@gmail.com>
2023-11-13 20:37:44 +0100

    add today's examples

---
+++ esercizi/06-flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  first_plates = [{'id': 1, 'name': 'Pasta al tonno'},{'id': 2, 'name': 'Lasagne'}, {'id': 3, 'name': 'Pasta al sugo'}]
+  return render_template('index.html', first=first_plates)
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+  
