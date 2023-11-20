89f2333808362d0149e531e2339884f92118386e
Luigi De Russis <luigidr@gmail.com>
2023-11-20 18:58:04 +0100

    add exercise done in class

--- esercizi/06-flask/esercizio/app.py
+++ esercizi/06-flask/esercizio/app.py
@@ -4,7 +4,24 @@ from flask import Flask, render_template
 # create the application
 app = Flask(__name__)
 
+menu = [
+  {'type': 'Primi', 'plates': [{'name': 'pasta al tonno', 'promotion': True}, {'name': 'pasta al sugo'}]},
+  {'type': 'Secondi', 'plates': [{'name': 'tonno', 'promotion': True}, {'name': 'cotoletta'}]},
+  {'type': 'Contorni', 'plates': [{'name': 'fagiolini'}, {'name': 'patate al forno'}]},
+  ]
+
 # define the homepage
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', menu=menu)
+
+# define the about page
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+# define the page of each first plates
+@app.route('/first/<int:id>')
+def first_plate(id): # indice del piatto nell'array "plates"
+  plate = menu[0]['plates'][id-1]
+  return render_template('single.html', plate=plate)
